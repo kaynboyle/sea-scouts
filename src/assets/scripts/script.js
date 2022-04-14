90fcc8aa8d2ee410a7196dc8ff6de9ada241542c
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-04-14 00:36:00 -0700

    icy lines using react hooks mous move react hook in scripts, html canvas working in contact index.js

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+
+export const useMousePosition = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+    const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+        window.addEventListener("mousemove", setFromEvent);
+    return () => {
+        this.window.removeEventListener("mousemove", setFromEvent)
+    
+    };
+},[]);
+    return position;
+    };
+
+
--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,10 +1,97 @@
 import React from 'react';
-import gif from "../../assets/ezgif-5-888cbcb2d7.gif";
-
+import { useEffect, useState } from "react";
+import { useMousePosition } from "../../assets/scripts/script.js"
 function ContactUs() {
+    const mouse = useMousePosition();
+    React.useEffect(() => {
+        const cvs = document.querySelector('canvas');
+        const c = cvs.getContext('2d');
+        cvs.width = window.innerWidth;
+        cvs.height = window.innerHeight;
+
+        window.addEventListener('resize', function () {
+        cvs.width = window.innerWidth;
+        cvs.height = window.innerHeight;
+        });
+        window.addEventListener('mousemove', function (e) {
+            mouse.x = e.x;
+            mouse.y = e.y;
+          });
+        class Line {
+            constructor(x, y, offset) {
+                this.x = x;
+                this.y = y;
+                this.offset = offset;
+                this.radians = 0;
+                this.velocity = 0.01;
+        }
+        draw = () => {
+            c.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+            c.fillStyle = 'rgba(255, 255, 255, 0.3)';
+        const drawLinePath = (width = 0, color) => {
+            c.beginPath();
+            c.moveTo(this.x - (width / 2), this.y + (width / 2));
+            c.lineTo(this.x - (width / 2) + 300, this.y - (width / 2) - 1000);
+            c.lineTo(this.x + (width / 2) + 300, this.y - (width / 2) - 1000);
+            c.lineTo(this.x + (width / 2), this.y - (width / 2));
+            c.closePath();
+            if (c.isPointInPath(mouse.x, mouse.y) && color) {
+                c.strokeStyle = color;
+            };
+        };
+        drawLinePath(150, '#baf2ef');
+        drawLinePath(50, '#dcf3ff');
+
+        c.beginPath();
+        c.arc(this.x, this.y, 1, 0, Math.PI * 2, false);
+        c.fill();
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.x + 300, this.y - 1000);
+        c.stroke();
+        c.closePath();
     
+        this.update();
+      }
+    
+      update = () => {
+        this.radians += this.velocity;
+        this.y = this.y + Math.cos(this.radians + this.offset);
+      }
+    }
+    
+    const lineArray = [];
+    
+    for (let i = 0; i < 100; i++) {
+    
+      const start = { x: -250, y: 800 };
+      const random = Math.random() - 0.5;
+      const unit = 25;
+    
+      lineArray.push(
+        new Line(
+          start.x + ((unit + random) * i),
+          start.y + (i + random) * -3 + Math.sin(i) * unit,
+          0.1 + (1 * i)
+        )
+      );
+    };
+    
+    function animate() {
+      requestAnimationFrame(animate);
+      c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      lineArray.forEach(line => {
+        line.draw();
+      })
+    };
+    
+    animate();
+    
+
+
+      }, []);
     return (
         <div className="App">
+            <canvas id="myCanvas"></canvas>
             <div className="flex1">
                 <div class="card">
                 <div class="card-body">
@@ -112,6 +199,7 @@ function ContactUs() {
                       <img src={gif} alt="cherry logo"/>
                 </div> */}
             </div>
+            <div id="page-extension"></div>
         </div>   
         
     );
